21727f8dcc04b76bd47cbc9b66ac0391ef3ad214
jinju <hor629@naver.com>
2024-01-14 16:52:00 +0900

    [Setting/#1]: 폰트 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import GlobalStyle from './styled/GlobalStyle';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <GlobalStyle />
+      <span>안녕</span>
+      <span>hello</span>
+    </div>
+  );
 }
 
 export default App;
