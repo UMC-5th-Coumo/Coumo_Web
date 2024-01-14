f6a082358162dc479bc828b82818fb503d245fa3
JIMIN1020 <jimins4920@gmail.com>
2024-01-14 20:25:53 +0900

    [Setting/#3]: 폰트 설정 수정, 컬러 변수 정의

---
+++ src/styles/theme.js
@@ -0,0 +1,10 @@
+export const COLORS = {
+  banner_gradient: 'linear-gradient(331deg, #7733F9 6.83%, #E6D9FF 114.92%)',
+  coumo_purple: '#7C43E8',
+  coumo_lightpurple: '#EDE4FC',
+  coumo_gray: '#E9E9E9',
+  text_darkgray: '#514D59',
+  text_gray: '#545252',
+  text_lightgray: '#B1B1B1',
+  white: '#FCFCFC',
+};
