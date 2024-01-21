c3b94dae9e3fb72fd22955bded4981eb7146b03f
JIMIN1020 <jimins4920@gmail.com>
2024-01-21 17:11:34 +0900

    [Feat/#31] 고객 데이터 관리 공통 컴포넌트

---
+++ src/components/admin/customer/index/Index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+import { COLORS } from '../../../../styles/theme';
+
+const Index = ({ text, onClickTab, isSelected }) => {
+  return (
+    <TabDiv onClick={onClickTab} selected={isSelected}>
+      {text}
+    </TabDiv>
+  );
+};
+
+export default Index;
+
+const TabDiv = styled.div`
+  width: ${(props) => (props.selected ? 160 : 136)}px;
+  height: 40px;
+  box-sizing: border-box;
+  padding: 12px 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: width ease-in-out 0.3s;
+
+  cursor: pointer;
+  border-radius: 0px 24px 24px 0px;
+  background: ${(props) =>
+    props.selected ? COLORS.coumo_purple : COLORS.btn_lightgray};
+  color: ${(props) => (props.selected ? COLORS.white_fff : COLORS.tab_gray)};
+  font-size: 16px;
+  font-weight: ${(props) => (props.selected ? '700' : '500')};
+  line-height: 132%;
+  letter-spacing: 0.48px;
+`;
--- src/pages/Customer.jsx
+++ src/pages/Customer.jsx
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import CustomerCard from '../components/admin/customer/CustomerCard';
+import {
+  customerTabMenu,
+  customerTabs,
+  indexMenu,
+} from '../assets/data/tabData';
+import GroupTabBar from '../components/admin/customer/groupTab/GroupTabBar';
+import TabMenuBar from '../components/admin/customer/tabMenu/TabMenuBar';
+import IndexBar from '../components/admin/customer/index/IndexBar';
 
 const Customer = () => {
-  return <Container>Customer</Container>;
+  const [card, setCard] = useState('1');
+  const [group, setGroup] = useState(customerTabs[0].key);
+  const [menu, setMenu] = useState(customerTabMenu[0].key);
+  const [index, setIndex] = useState(indexMenu[0].key);
+  return (
+    <Container>
+      <GroupTabBar
+        tabs={customerTabs}
+        selected={group}
+        setSelected={setGroup}
+      />
+      {customerDummyData.map((data) => {
+        return (
+          <CustomerCard
+            key={data.id}
+            data={data}
+            selected={card}
+            setSelected={setCard}
+          />
+        );
+      })}
+      <TabMenuBar
+        tabs={customerTabMenu}
+        selected={menu}
+        setSelected={setMenu}
+      />
+      <IndexBar tabs={indexMenu} selected={index} setSelected={setIndex} />
+    </Container>
+  );
 };
 
 export default Customer;
@@ -11,3 +48,38 @@ const Container = styled.div`
   box-sizing: border-box;
   padding: 70px 120px;
 `;
+
+const customerDummyData = [
+  {
+    id: '1',
+    name: '강수빈',
+    number: '010-0000-0000',
+    gender: '여성',
+    totalStamp: '4',
+    recentVisit: '23.11.04',
+  },
+  {
+    id: '2',
+    name: '강수빈',
+    number: '010-0000-0000',
+    gender: '여성',
+    totalStamp: '4',
+    recentVisit: '23.11.04',
+  },
+  {
+    id: '3',
+    name: '강수빈',
+    number: '010-0000-0000',
+    gender: '여성',
+    totalStamp: '4',
+    recentVisit: '23.11.04',
+  },
+  {
+    id: '4',
+    name: '강수빈',
+    number: '010-0000-0000',
+    gender: '여성',
+    totalStamp: '4',
+    recentVisit: '23.11.04',
+  },
+];
