7db595ae55e8e0b024f12b1eb957f87c10a9017e
JIMIN1020 <jimins4920@gmail.com>
2024-01-28 23:06:00 +0900

    [API/#53]: axios instance 생성

---
+++ src/api/axios.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const BASE_URL = 'server 주소';
+
+// 인증 값 필요없는 경우
+export const defaultInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// 인증 값 필요, json
+const token = 'access token';
+export const authInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: 'Bearer ' + token,
+  },
+});
+
+// 인증 값 필요, FormData
+export const formAuthInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'multipart/form-data',
+    Authorization: 'Bearer ' + token,
+  },
+});
