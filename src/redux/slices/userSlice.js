5e840c4ac023cf60ffdaa62498f87d14c4466b47
jinju <hor629@naver.com>
2024-02-14 13:25:28 +0900

    [Fix/#199]: 주소 검색 API 오류 해결

--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -2,13 +2,13 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   name: '',
-  email: '',
-  phone: '',
   id: '',
   pw: '',
   ownerId: '',
   storeId: '',
   token: '',
+  createdAt: '',
+  write: '',
 };
 
 const userSlice = createSlice({
@@ -17,13 +17,13 @@ const userSlice = createSlice({
   reducers: {
     setUser: (state, action) => {
       state.name = action.payload.name;
-      state.email = action.payload.email;
-      state.phone = action.payload.phone;
       state.id = action.payload.id;
       state.pw = action.payload.pw;
       state.ownerId = action.payload.ownerId;
       state.storeId = action.payload.storeId;
       state.token = action.payload.token;
+      state.createdAt = action.payload.createdAt;
+      state.write = action.payload.write;
     },
   },
 });
