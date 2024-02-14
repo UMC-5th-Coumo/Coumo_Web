f8643b7662b0aedca616069584b0896371424f9d
jinju <hor629@naver.com>
2024-02-15 04:00:47 +0900

    [Style/#217]: 쿠폰 UI 서비스 step 반응형 수정

--- src/components/admin/coupon/CouponStep.jsx
+++ src/components/admin/coupon/CouponStep.jsx
@@ -25,6 +25,10 @@ const Step = styled.div`
   gap: 8px;
   border-radius: 24px;
 
+  @media screen and (max-width: 1440px) {
+    width: 230px;
+  }
+
   @media screen and (max-width: 1024px) {
     width: 196px;
   }
@@ -62,7 +66,11 @@ const Divider = styled.div`
   transform: translateY(-50%);
   z-index: 0;
 
-  @media screen and (max-width: 1600px) {
+  @media screen and (max-width: 1440px) {
+    width: 230px;
+  }
+
+  @media screen and (max-width: 1300px) {
     opacity: 0;
   }
 `;
